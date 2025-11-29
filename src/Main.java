03de80c0e8d29e544f73aaf5fd020887217bdef7
Carlos <cialvaradoz@udistrital.edu.co>
2025-11-29 17:30:33 -0500

    Solo el menú

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int opcion=0;
+        Scanner leer = new Scanner(System.in);
+
+        do{
+            System.out.println("      Bienvenido al menú      ");
+            System.out.println("Elige una opción");
+            System.out.println("1). Triqui");
+            System.out.println("2). Parejas");
+            System.out.println("3). Ahorcado");
+            System.out.println("4). Calculadora");
+            System.out.println("5). Salir del menú");
+            opcion = leer.nextInt();
+
+            switch(opcion){
+                case 1: System.out.println(" Has elegido: Triqui");
+
+                    break;
+                case 2: System.out.println(" Has elegido: Parejas");
+
+                    break;
+                case 3: System.out.println(" Has elegido: Ahorcado");
+
+                    break;
+                case 4: System.out.println(" Has elegido: Calculadora");
+
+                    break;
+
+            }
+        }while(opcion != 5);
+        System.out.println("Hasta luego");
+    }
+}
